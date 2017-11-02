330b3586d633f62e60e57d8052d131e07a78de01
Venugopal Ramamurthy <venugopal.ramamurthy@flex.com>
2017-11-01 23:21:07 -0700

    [Extension] Update application settings

--- server/config.js
+++ server/config.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
     // Start URL that browsing should start at.
-    startURL: 'http://gdlnte811.americas.ad.flextronics.com/Xtend/AFS/info/Batch',
+    startURL: 'http://gdlnte811.americas.ad.flextronics.com/Xtend/AFS/info',
     loginURL: 'http://your-application-url-with-basic-login',
     // Start Method when there is no start URL.  This is fpr when we are doing an API integration,
     // and we don't actually need to load phantom (yet).
